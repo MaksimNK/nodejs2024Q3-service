d962673826e043ef13839181daa80879fc08d902
MaksimNK <maksmozir@gmail.com>
2024-11-11 20:43:02 +0300

    feat: add port in env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { config } from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  config();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
